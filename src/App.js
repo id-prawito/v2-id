e8c5a3f57a46153ac823a24a2535ec2ace5f9a2c
Prawito <prawito.id@gmail.com>
2022-01-07 00:02:11 +0700

    gh-pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
             <h1>
                 React App Axios GH-pages framer-motion rc-tabs react-icons
                 react-intersection-observer react-router-dom react-scroll
+                react-tilt sass styled-components
             </h1>
         </div>
     );
