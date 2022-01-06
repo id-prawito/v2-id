965f87db0908b63376666e6387ce694ac9b21025
Prawito <prawito.id@gmail.com>
2022-01-06 23:31:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <h1>React App Axios GH-pages framer-motion</h1>
+            <h1>
+                React App Axios GH-pages framer-motion rc-tabs react-icons
+                react-intersection-observer
+            </h1>
         </div>
     );
 }
