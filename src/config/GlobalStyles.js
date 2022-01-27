f5b18f42d3713316cde3087574b779bec4625cb6
Prawito <prawito.id@gmail.com>
2022-01-27 23:35:07 +0700

    fix-bugs

--- src/config/GlobalStyles.js
+++ src/config/GlobalStyles.js
@@ -3,29 +3,22 @@ import themeList from "./themeList";
 
 const GlobalStyles = createGlobalStyle`
   :root{
+    // Color
     --dark-background: #1A1D29;
     --light-background: #f0f8ff;
-
     --mediumSlateBlue: #6C62E2;
     --dark-components: #7C3AED; // Ungu
-
     --darkCardComponent : #7C3AED; // Ungu
     --lightCardComponent : #1f2937;
-
     --light-text-black: #000000;
     --dark-text-white: #FFFFFF; // White
-
     --light-text-grey: #979797;
-
     --lightText: #3c3c3c;
     --darkText: #ccd6f6;
-
     --lightTab : #1f2937;
     --darkTab : #979797;
-
     --colorMain : #7c3aed; // Ungu
     --colorWhite : #ffffff; // White
-
   }
 
   body {
---
+++ src/config/_images.js
@@ -0,0 +1,49 @@
+import illustrator from "../assets/img/tech_stack/illustrator.png";
+import javascript from "../assets/img/tech_stack/javascript.svg";
+import typescript from "../assets/img/tech_stack/typescript.svg";
+import visualCode from "../assets/img/tech_stack/visualCode.png";
+import photoshop from "../assets/img/tech_stack/photoshop.svg";
+import firebase from "../assets/img/tech_stack/firebase.svg";
+import reactJs from "../assets/img/tech_stack/reactJS.svg";
+import mongoDB from "../assets/img/tech_stack/mongoDB.svg";
+import github from "../assets/img/tech_stack/github.svg";
+import nextJS from "../assets/img/tech_stack/nextJS.svg";
+import sqlite from "../assets/img/tech_stack/sqlite.svg";
+import nodeJS from "../assets/img/tech_stack/nodeJS.svg";
+import mySQL from "../assets/img/tech_stack/mySQL.svg";
+import vueJs from "../assets/img/tech_stack/vueJs.svg";
+import figma from "../assets/img/tech_stack/figma.svg";
+import html5 from "../assets/img/tech_stack/html.svg";
+import scss from "../assets/img/tech_stack/scss.svg";
+import css from "../assets/img/tech_stack/css.svg";
+import php from "../assets/img/tech_stack/php.svg";
+import git from "../assets/img/tech_stack/git.svg";
+import pokedex from "../assets/img/pokedex-id.PNG";
+import disneyPlus from "../assets/img/disney-plus.PNG";
+import spkPromethee from "../assets/img/spk-promethee.PNG";
+
+export {
+    illustrator,
+    javascript,
+    typescript,
+    visualCode,
+    photoshop,
+    firebase,
+    reactJs,
+    mongoDB,
+    github,
+    nextJS,
+    sqlite,
+    nodeJS,
+    mySQL,
+    vueJs,
+    figma,
+    html5,
+    scss,
+    css,
+    php,
+    git,
+    pokedex,
+    disneyPlus,
+    spkPromethee,
+};
